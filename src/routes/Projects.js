76004ed84a89ae4d0f2a3d76a15248724db06c91
Sam Verdejo <sjverdejo@gmail.com>
2023-06-11 13:21:35 -0400

    Updated projects page

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,7 +1,8 @@
 import '../assets/styles/Container.css'
 import Header from '../components/Header'
 import ProjectContent from '../components/ProjectContent'
-import img from '../assets/images/projects/portfolio.png'
+import portfolio from '../assets/images/projects/portfolio.png'
+import taskme from '../assets/images/projects/taskme.png'
 import NavLink from '../components/NavBar/NavLink'
 
 function Projects() {
@@ -15,15 +16,23 @@ function Projects() {
                 <div className='ems_proj'>
                     <ProjectContent 
                         title='Portfolio Website' 
-                        images={img}
+                        images={portfolio}
                         description='Website for personal project created using React, CSS, React-Router v6, and framer motion'
                         ghLink='https://github.com/sjverdejo/Portfolio'
                     />
                     <hr />
 
                     <ProjectContent 
+                        title='TaskME' 
+                        images={taskme}
+                        description='A simple CRUD application for a user to track tasks to be completed and when to be completed by. Made using React, Express, Node, PostgreSQL, HTML, CSS.'
+                        ghLink='https://github.com/sjverdejo/TaskME'
+                    />
+                    <hr />
+
+                    <ProjectContent 
                         title='Employee Manager' 
-                        images={img}
+                        images={portfolio}
                         description='An application to help employers manage their employees. Created using React, Express, PostgreSQL, Node, TailwindCSS'
                         ghLink='https://github.com/sjverdejo/Employee-Management-System'
                     />
